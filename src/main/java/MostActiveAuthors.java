c662bf187c273640b582af39c8ae0d38aeea8bbe
netajik <kancharapunetaji@gmail.com>
2023-08-20 10:27:05 +0530

    most active authors problem solution implemented

---
+++ src/main/java/MostActiveAuthors.java
@@ -0,0 +1,72 @@
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Most Active Authors
+ *
+ * API URL: https://jsonmock.hackerrank.com/api/article_users?page=<pageNumber>
+ * threshold: integer that represents the threshold value for the number of
+ * submission count
+ * The function should return an array of strings that represent the usernames
+ * of users whose submission count is greater
+ * than the given threshold. The usernames in the array must be ordered in the
+ * order they appear in the API response
+ *
+ */
+
+public class MostActiveAuthors {
+    public static void main(String[] args) throws IOException {
+        int threshold = 10;
+        List<String> users = null;
+        try {
+            users = getUserNames(threshold);
+        } catch (MalformedURLException | ParseException e) {
+            e.printStackTrace();
+        }
+        users.stream().forEach(System.out::println);
+    }
+
+    static List<String> getUserNames(int threshold) throws IOException, ParseException {
+        // set variables
+        List<String> users = new ArrayList<>();
+        Long page = 1l, totalPages = 1l;
+
+        while (page <= totalPages) {
+            URL url = new URL("https://jsonmock.hackerrank.com/api/article_users?page=" + page);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.addRequestProperty("Content-Type", "application/json");
+            int status = connection.getResponseCode();
+            if (status < 200 || status > 300) {
+                throw new IOException("Exception");
+            }
+            JSONParser parser = new JSONParser();
+            JSONObject responseObj = (JSONObject) parser.parse(new InputStreamReader(connection.getInputStream()));
+            if(page==1){
+                totalPages = (Long)responseObj.get("total_pages");
+            }
+            JSONArray articals = (JSONArray) responseObj.get("data");
+            for(Object artical:articals){
+                JSONObject articalObj = (JSONObject) artical;
+                Long submissinCount = (Long) articalObj.get("submission_count");
+                String userName = (String) articalObj.get("username");
+                if(submissinCount>threshold){
+                    users.add(userName);
+                }
+            }
+            page++;
+        }
+        return users;
+    }
+}
